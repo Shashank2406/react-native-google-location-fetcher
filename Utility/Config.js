001e97c1a3a320e12cc9bd3227ecd7ba9d09ab94
Shashank Srivastava <shashank.srivastava1@kelltontech.com>
2018-07-15 18:59:47 +0530

    Added Google API's

---
+++ Utility/Config.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * Configuration for API
+ * @author Shashank Srivastava
+ *
+ */
+
+ export const CONFIG = {
+    locationApiUrl: {
+        getGoogleAddress: `https:/maps.googleapis.com/maps/api/geocode/json?`,
+        getGoogleCoordinates: `https:/maps.googleapis.com/maps/api/geocode/json?`
+    }
+}
